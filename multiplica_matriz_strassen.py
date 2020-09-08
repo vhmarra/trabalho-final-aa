3bc05e9364ebf939352f009ed4c6ad2736a1c167
Victor Hugo Marra Araujo <31578843+vhmarra@users.noreply.github.com>
2020-09-08 17:10:32 -0300

    Add files via upload
    retorno das matrizes está certo agora

--- multiplica_matriz_strassen.py
+++ multiplica_matriz_strassen.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 sys.setrecursionlimit(90000000)
 
-matrix_size = 4
+matrix_size = 8
 valor_maximo = 6
 
 matriz1_strassen = np.random.randint(0,valor_maximo, size=(matrix_size,matrix_size), dtype=np.longlong) # Gera matriz1 com numeros aleatorios de 0 a 10001
@@ -24,23 +24,36 @@ def subtrai_matriz(matriz1,matriz2):
     print("\n")
     return matriz1 - matriz2
 
-def divide_matriz(matriz):
+def divide_matriz(matriz,matrix_size):
     if len(matriz) % 2 != 0 or len(matriz[0]) % 2 != 0:
         raise Exception("Somente matrizes pares")
 
     tamanho = len(matriz)
     meio = tamanho//2  
-    topo_esquerdo = [matriz[linha][coluna] for coluna in range(meio) for linha in range(meio)] 
-    base_esquerdo = [matriz[linha][coluna] for coluna in range(meio) for linha in range(meio, tamanho)] 
-    topo_direito  = [matriz[linha][coluna] for coluna in range(meio,tamanho) for linha in range(meio)]  
-    base_direito  = [matriz[linha][coluna] for coluna in range(meio,tamanho) for linha in range(meio,tamanho)]
+    topo_esquerdo = [matriz[linha][coluna] for linha in range(meio) for coluna in range(meio)] 
+    base_esquerdo = [matriz[linha][coluna] for linha in range(meio) for coluna in range(meio, tamanho)] 
+    topo_direito  = [matriz[linha][coluna] for linha in range(meio,tamanho) for coluna in range(meio)]  
+    base_direito  = [matriz[linha][coluna] for linha in range(meio,tamanho) for coluna in range(meio,tamanho)]
 
-    return topo_esquerdo,topo_direito,base_esquerdo,base_direito
+    te = np.reshape(topo_esquerdo,(-1,matrix_size//2))
+    be = np.reshape(base_esquerdo,(-1,matrix_size//2))
+    td = np.reshape(topo_direito,(-1,matrix_size//2))
+    bd = np.reshape(base_direito,(-1,matrix_size//2))
 
+    return te,be,td,bd
 
 # print(soma_matriz(matriz1_strassen,matriz2_strassen))   
 # print(subtrai_matriz(matriz1_strassen,matriz2_strassen))
 
 print(matriz1_strassen)
 print("\n")
-print(divide_matriz(matriz1_strassen))         
+a,b,c,d = divide_matriz(matriz1_strassen,matrix_size)
+
+print(a)
+print("\n")
+print(b)
+print("\n")
+print(c)
+print("\n")
+print(d)
+
